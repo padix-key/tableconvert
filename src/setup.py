67e5438e3c60ab70e47889aabf0347afb5bf5fb2
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-05-02 16:57:18 +0200

    Added license file

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='tableconvert',
-      version='0.1',
-      description='',
-      url='',
-      author='',
-      author_email='',
-      license='MIT',
-      packages=['numpy'],
+setup(name="tableconvert",
+      version="0.1",
+      description="This package simply converts pasted spreadsheet data into a numpy array",
+      url="https://github.com/padix-key/tableconvert",
+      author="Patrick Kunzmann",
+      license="MIT",
+      packages=["numpy"],
       zip_safe=False)
